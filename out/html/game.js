779790b6b76c26051d93b22998147514bf4d47cd
Gerenalnyaim <Victordanko2020@gmail.com>
2025-07-07 18:10:24 +0200

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -49,6 +49,14 @@
         window.dendryUI.dendryEngine.goToScene('mod_loader');
     }
   };
+
+  window.showSandbox = function() {
+      if (window.dendryUI.dendryEngine.state.sceneId.startsWith('sandbox')) {
+          window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+      } else {
+          window.dendryUI.dendryEngine.goToScene('sandbox');
+      }
+  };
   
   window.showOptions = function() {
       var save_element = document.getElementById('options');
